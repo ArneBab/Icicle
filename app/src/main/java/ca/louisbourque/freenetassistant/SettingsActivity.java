08c735f49e8b1b154aacfa4aea919581407de121
Louis Bourque <louisrbourque@gmail.com>
2015-01-21 19:04:45 -0700

    Add toolbar to Settings Activity

--- app/src/main/java/ca/louisbourque/freenetassistant/SettingsActivity.java
+++ app/src/main/java/ca/louisbourque/freenetassistant/SettingsActivity.java
@@ -2,14 +2,19 @@ package ca.louisbourque.freenetassistant;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
 
-
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        // Set up the action bar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        // setHasOptionsMenu(true);
+        setSupportActionBar(toolbar);
         setResult(Activity.RESULT_OK);
     }
 }
